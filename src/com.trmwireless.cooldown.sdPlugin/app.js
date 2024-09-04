d22b758d6997ff45182b3c1df89243b05a8e1e26
tmarks <tmarks@gmail.com>
2024-09-03 20:18:19 -0500

    notworking

--- src/com.trmwireless.cooldown.sdPlugin/app.js
+++ src/com.trmwireless.cooldown.sdPlugin/app.js
@@ -2,6 +2,7 @@
 /// <reference path="libs/js/stream-deck.js" />
 
 const myAction = new Action('com.trmwireless.cooldown.cooldownTimer');
+const CLEAR_SCREEN_DELAY_MS = 3000;
 
 /**
  * The first event fired when Stream Deck starts
@@ -11,9 +12,27 @@ $SD.onConnected(({ actionInfo, appInfo, connection, messageType, port, uuid }) =
 });
 
 myAction.onKeyUp(({ action, context, device, event, payload }) => {
+	
 	console.log('Your key code goes here!');
+	var c = document.getElementById("cooldownCanvas");
+	var ctx = c.getContext("2d");
+	ctx.fillStyle = RGB(255, 0, 0);
+	ctx.globalAlpha = 0.5;
+	ctx.fillRect(0, 0, 144, 144);
+
+	var timeout = timeoutPromise(CLEAR_SCREEN_DELAY_MS).then(() => {
+		ctx.clearRect(0, 0, 144, 144);
+	});
 });
 
 myAction.onDialRotate(({ action, context, device, event, payload }) => {
 	console.log('Your dial code goes here!');
 });
+
+function timeoutPromise(duration) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, duration);
+    });
+}
